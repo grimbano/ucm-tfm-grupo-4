eb4841f7c5984dff095581546ee91743cbcd8e4b
grimbano <gastonrimbano@gmail.com>
2025-07-28 21:33:45 +0200

    feat(repository): :wrench: Add separate service in docker compose for db restoring

--- data/database/postgres/docker/restore_on_startup.sh
+++ data/database/postgres/docker/restore_on_startup.sh
@@ -8,7 +8,7 @@ echo ""
 # Espera pasiva, ya que depends_on: service_healthy ya debería haber hecho el trabajo
 # Sin embargo, un pg_isready final no hace daño.
 echo "Verifying PostgreSQL connectivity..."
-until pg_isready -h "$PGHOST" -p 5432 -U "$PGUSER" -q; do
+until pg_isready -q; do
   echo "PostgreSQL not yet responsive, waiting..."
   sleep 2
 done
@@ -17,14 +17,14 @@ echo "PostgreSQL is responsive. Proceeding with restoration."
 
 echo ""
 
-echo "Restoring database '${PGDATABASE}' from ${DUMP_FILE} ..."
-pg_restore -Fc -v -d "$PGDATABASE" -U "$PGUSER" "${DUMP_FILE}"
+echo "Restoring database '${PGDATABASE}' from ${BAK_FILE} ..."
+pg_restore -Fc -v -d "${PGDATABASE}" "${BAK_FILE}"
 echo "Restoration completed."
 
 echo ""
 
 echo "Deleting 'public' schema if exists in '${PGDATABASE}' ..."
-psql -d "$PGDATABASE" -U "$PGUSER" -c "DROP SCHEMA IF EXISTS public;"
+psql  -d "${PGDATABASE}" -c "DROP SCHEMA IF EXISTS public;"
 echo "Deleation completed."
 
 echo ""
